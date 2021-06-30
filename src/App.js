d80726a447405d5f881bb7089648bc925076bdd3
Keith March Mistler <kmistler@gmail.com>
2021-06-30 11:16:06 -0400

    search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+const api ={
+  key: "e4d33e4eba8f5159627ebf500dc4b54e",
+  base: "https://api.openweathermap.org/data/2.5"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+      </div>
+      </main>
     </div>
   );
 }
