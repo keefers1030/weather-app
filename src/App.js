f4f48cb9f05576923126fa24b923298a138616ba
Keith March Mistler <kmistler@gmail.com>
2021-06-30 11:54:06 -0400

    added date function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import './index.css'
 //   base: "https://api.openweathermap.org/data/2.5"
 // }
 
+const dateBuilder = (d) => {
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+let day = days[d.getDay()]
+let date = d.getDate()
+let month = months.[d.getMonth()]
+let year = d.getFullYear()
+
+return `${day} ${date} ${month} ${year}`
+}
+
 function App() {
   return (
     <div className="app">
@@ -16,6 +28,11 @@ function App() {
           className="search-bar"
           placeholder="Search..."
           />
+      </div>
+      <div className="location-box">
+
+        <div className="location">New York City, US</div>
+        <div className="date">{dateBuilder(new Date())}</div>
       </div>
       </main>
     </div>
